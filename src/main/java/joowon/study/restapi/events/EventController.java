b355357ad9c84b13e2c49c3d3f37087377e5c44f
JOOWON-PC <destiny0712@naver.com>
2019-01-15 20:35:10 +0900

    ModelMapper와 EventDto를 만들어 입력값 제한하기. 테스트 코드 변경(스프링부트 통합 환경)

--- src/main/java/joowon/study/restapi/events/EventController.java
+++ src/main/java/joowon/study/restapi/events/EventController.java
@@ -1,5 +1,6 @@
 package joowon.study.restapi.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,17 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
 
-    @Autowired
-    EventRepository eventRepository;
+    private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
+        this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
+    }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createUri).body(event);
