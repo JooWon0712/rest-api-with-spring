ac1e5f3d3afac6d38f7ef799068c0f91d5209a93
JOOWON-PC <destiny0712@naver.com>
2019-01-16 16:33:53 +0900

    api 인덱스 만들기 공부 event api 응답중 오류가 발생하였을 경우 index로 이동할수 있는 링크 추가하는 방법.

--- src/main/java/joowon/study/restapi/events/EventController.java
+++ src/main/java/joowon/study/restapi/events/EventController.java
@@ -1,5 +1,6 @@
 package joowon.study.restapi.events;
 
+import joowon.study.restapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -34,13 +35,13 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
 
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -54,5 +55,9 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createUri).body(eventResource);
+    }
+
+    private ResponseEntity<ErrorsResource> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
